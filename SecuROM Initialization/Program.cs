9e4b6cc7bda378d742fd23f986274040875ad1c0
BlackAnt1968601851 <117094394+BlackAnt1968601851@users.noreply.github.com>
2024-01-21 02:15:36 -0500

    Add files via upload

---
+++ SecuROM Initialization/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using Steamworks;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (SteamAPI.Init())
+            {
+                var GTAIV = "SteamCheck.exe";
+                if (File.Exists(GTAIV) == true)
+                {
+                    var v = System.AppDomain.CurrentDomain.BaseDirectory;
+                    Process.Start(GTAIV, "/immediate 23\\10\\2009 15:18:59" + " \"" + v + "LaunchGTAIV.exe\"");
+                    Environment.Exit(0);
+                }
+                else
+                {
+                    Console.WriteLine("Error Code: 3\nYou seem to be missing SteamCheck.exe.");
+                    Console.ReadKey();
+                    Environment.Exit(3);
+                }
+            }
+            if (SteamAPI.IsSteamRunning() == false)
+            {
+                Console.Clear();
+                Console.WriteLine("Steam DRM has failed Error: 1");
+                Console.WriteLine("You need to start Steam.");
+                Console.ReadKey();
+                SteamAPI.Shutdown();
+                Environment.Exit(1);
+            }
+            if (!(SteamAPI.Init()))
+            {
+                {
+                    Console.Clear();
+                    Console.WriteLine("Steam DRM has failed Error: 2");
+                    Console.WriteLine("You need to purchase GTA IV on steam.");
+                    Console.WriteLine("or launch the game from steam.");
+                    Console.ReadKey();
+                    SteamAPI.Shutdown();
+                    Environment.Exit(2);
+                }
+            }
+        }
+    }
+}
